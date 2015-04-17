a72c9850f19f0a4e69a767435a623672dfee6f8d
James Pike <jpike@chilon.net>
2015-04-17 21:45:43 +0100

    Now works together with process killing/caching.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,41 @@
-export default function(op, opts) {
-  return op.stream.map(events => {
-    console.log('TODO: run mocha tests')
-    return events
+export default function(op, opts = {}) {
+  var { Bacon } = op
+
+  var mochaProc = op.procPool.prepare(opts => {
+    var Mocha = require('mocha')
+    var Promise = require('bluebird')
+    var _ = require('lodash')
+    var glob = Promise.promisify(require('glob'))
+
+    var files = opts.files
+    var initRequireCache = _.clone(require.cache)
+    delete opts.files
+
+    return () => {
+      Object.keys(require.cache).forEach(key => {
+        if (! initRequireCache[key])
+          delete require.cache[key]
+      })
+      var mocha = new Mocha(_.clone(opts))
+
+      console.log('run mocha tests %s', process.pid)
+      return glob(files).then(fileList => {
+        fileList.forEach(file => mocha.addFile(file))
+
+        return new Promise(resolve => {
+          mocha.run(nFailures => resolve(nFailures))
+        })
+      })
+      .then(nFailures => {
+        // console.log('finish mocha tests', process.pid)
+        return process.pid
+      })
+    }
+  }, opts, { processLimit: 2 })
+
+  return op.stream.flatMapLatest(events => {
+    mochaProc.kill()
+
+    return Bacon.fromPromise(mochaProc().then(() => events))
   })
 }
